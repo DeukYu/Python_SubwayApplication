ec06092912343a21e50a3e9778651eb0b6cc9a73
DeokgyuKim <kduck119@gmail.com>
2019-05-20 16:54:42 +0900

    Add StationInfoFind
    1.  jsonParsing_FindStation
    2. main_Add_self.Radiovariety.get()==3
    3. main_FindStation
    4. main_MapView

--- jsonParsing.py
+++ jsonParsing.py
@@ -1,8 +1,10 @@
-import urllib
-import json
-import datetime
+import urllib.request, json, urllib.parse
 
-#url =
-u = urllib.urlopen(url)
-data = json.load(u.read())
-print(data)
+#서울시 지하철 역사 정보 조회
+def FindStation(stationName):
+    base_url = 'http://swopenAPI.seoul.go.kr/api/subway/49556d7755726c6137384e5475776f/json/stationInfo/0/10/'
+    path = urllib.parse.quote(stationName)
+    with urllib.request.urlopen(base_url + path) as url:
+        data = json.loads(url.read().decode('utf-8'))
+        print(data)
+        return data
--- main.py
+++ main.py
@@ -1,20 +1,57 @@
 from tkinter import *
+from jsonParsing import FindStation
+import folium
 
 class tkSubway:
+    def MapView(self, x, y, Info):
+        map_osm = folium.Map(location=[x, y], zoom_start=13)
+        folium.Marker([x, y], popup=Info).add_to(map_osm)
+        map_osm.save('osm.html')
+    def FindStation(self):
+        self.frame4.destroy()
+        self.frame4 = Frame(self.frame2, bd=2)
+        self.frame4.pack(side="right", fill="both", expand=True)
+        stationName = self.StationNameEntry.get()
+        stationInfoDict = FindStation(stationName)
+        totalcount = stationInfoDict['stationList'][0]['totalCount']
+        yValue = 10
+        Label(self.frame4, text="지하철 역명 : " + stationInfoDict['stationList'][0]['statnNm'] + "(" + stationInfoDict['stationList'][0]['statnNmEng'] + ")").place(x=10, y=yValue)
+        yValue += 30
+        for i in range(totalcount):
+            Label(self.frame4, text="지하철 호선 : " + stationInfoDict['stationList'][i]['subwayNm']).place(x=10, y=yValue)
+            yValue += 30
+            Label(self.frame4, text="전역 : " + stationInfoDict['stationList'][i]['statnFnm'] + ", 다음역 : " +stationInfoDict['stationList'][i]['statnTnm']).place(x=10, y=yValue)
+            yValue += 30
+        Label(self.frame4, text="소속 : " + stationInfoDict['stationList'][0]['operPblinstt']).place(x=10, y=yValue)
+        yValue += 30
+        Label(self.frame4, text="위치 : " + stationInfoDict['stationList'][0]['adresBass']).place(x=10, y=yValue)
+        yValue += 30
+        Label(self.frame4, text="전화번호 : " + stationInfoDict['stationList'][0]['telno']).place(x=10, y=yValue)
+        yValue += 30
+        Button(self.frame4, text="지도보기", command=lambda : self.MapView(stationInfoDict['stationList'][0]['subwayXcnts'],
+                                                                       stationInfoDict['stationList'][0]['subwayYcnts'],
+                                                                       stationInfoDict['stationList'][0]['statnNmEng'])).place(x=10, y=yValue)
     def check(self):
-        print(self.RadioVariety.get())
+        #print(self.RadioVariety.get())
         self.frame2.destroy()
         self.frame2 = Frame(self.window, bd=2, relief="solid")
         self.frame2.pack(side="bottom", fill="both", expand=True)
         if(self.RadioVariety.get() == 1):
             Label(self.frame2, image=self.Subway).pack()
-        if(self.RadioVariety.get() == 2):
+        if(self.RadioVariety.get() == 2):       #가까운 역 찾기
             pass
-        if (self.RadioVariety.get() == 3):
+        if (self.RadioVariety.get() == 3):      #역 정보 검색
+            self.frame3 = Frame(self.frame2, bd=2, relief="solid")
+            self.frame3.pack(side="left", fill="both", expand=True)
+            self.frame4 = Frame(self.frame2, bd=2)
+            self.frame4.pack(side="right", fill="both", expand=True)
+            Label(self.frame3, text="역 명 입력").place(x=10, y=10)
+            self.StationNameEntry = Entry(self.frame3)
+            self.StationNameEntry.place(x=13, y=40, height=20)
+            Button(self.frame3, text="확인", command=self.FindStation).place(x=160, y=37)
+        if (self.RadioVariety.get() == 4):      #역 구간 정보
             pass
-        if (self.RadioVariety.get() == 4):
-            pass
-        if (self.RadioVariety.get() == 5):
+        if (self.RadioVariety.get() == 5):      #분실물 검색
             pass
 
     def __init__(self):
@@ -25,6 +62,7 @@ class tkSubway:
         frame1.pack(side="top", fill="both", expand=False)
         self.frame2 = Frame(self.window, bd=2, relief="solid")
         self.frame2.pack(side="bottom", fill="both", expand=True)
+        self.StationNameEntry = Entry(self.frame2)
         Title = PhotoImage(file="source\\Title.png")
         SubTitle = []
         for i in range(5):
