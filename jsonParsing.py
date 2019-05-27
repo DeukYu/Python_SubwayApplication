e5343005b22a800ab6cf69e93d9be2db242af3a4
DeukYu <39644774+DeukYu@users.noreply.github.com>
2019-05-27 13:08:52 +0900

    분실물 파싱
    분실물 파싱중

--- jsonParsing.py
+++ jsonParsing.py
@@ -1,4 +1,4 @@
-import urllib.request, json, urllib.parse, pprint
+import urllib.request, json, urllib.parse, pprint, http.client
 
 #서울시 지하철 역사 정보 조회
 def FindStation(stationName):
@@ -32,3 +32,23 @@ def FindStationUseRate(stationName):
         data = json.loads(url.read().decode('utf-8'))
         print(data)
         return data
+
+# 서울시 대중교통 보관 분실물 검색
+# 습득물분류: 지갑, 쇼핑백, 서류봉투, 가방, 배낭, 핸드폰, 옷, 책, 파일, 기타
+# 습득물코드: s1(1~4호선), s2(5~8호선), s3(코레일), s4(9호선)
+# Article, station
+def Lost_Article():
+    key = '4c566371676c64793334654f5a6a7a/'
+    Article = urllib.parse.quote('핸드폰')
+    SubwayCode = urllib.parse.quote('s1')
+    base_url = 'http://openAPI.seoul.go.kr:8088/4c566371676c64793334654f5a6a7a/xml/SearchLostArticleService/1/5/'
+    #path = urllib.parse.quote_plus('')
+    print(base_url)
+    with urllib.request.urlopen(base_url + Article + '/' + SubwayCode) as url:
+        #print(url)
+        data = json.loads(url.read().decode('utf-8'))
+        print(data)
+        return data
+
+#FindStationFirstLast('정왕')
+Lost_Article()
